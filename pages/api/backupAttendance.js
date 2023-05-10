d487cf1ad73fff0da90011609f0d517acb2a846b
Md. Al-Sajiduzzaman Akand <alsajiduzzaman@outlook.com>
2023-05-10 20:50:29 +0600

    Update backupAttendance.js

--- pages/api/backupAttendance.js
+++ pages/api/backupAttendance.js
@@ -12,6 +12,10 @@ export default async function handler(req, res) {
         if (insert.acknowledged && insert.insertedCount > 0) {
             res.status(200).json({ message: "success" })
             return;
+        } else {
+            res.status(500).json({ message: "failed" })
+            console.log(insert)
+            return;
         }
     }
     catch (error) {
